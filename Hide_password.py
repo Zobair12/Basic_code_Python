d836f3ebb371de0f8d8f6c9b15eff6caa59c2e49
Zobair12 <alamjobayer5@gmail.com>
2025-09-30 22:17:33 +0600

    Hide password

---
+++ Hide_password.py
@@ -0,0 +1,8 @@
+import getpass
+
+username = input("Enter your username: ") 
+password = getpass.getpass("Enter your password: ")
+
+print("\nLogging in...")
+print("Usernam:", username)
+print("Password: [hidden for security]")
