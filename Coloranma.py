1d2e47a4a7df39394c09776594a00ffea424ffc9
Zobair12 <alamjobayer5@gmail.com>
2025-10-03 06:35:20 +0600

    Add Colors

---
+++ Coloranma.py
@@ -0,0 +1,7 @@
+from colorama import Fore, Style 
+
+print(Fore.RED + "Error: Something wrong")
+print(Fore.GREEN + "Success: Successfully")
+print(Style.RESET_ALL+"Back to normal")
+print(Fore.YELLOW + "Warning: Warning message")
+print(Fore.CYAN + "Info: Information message")
